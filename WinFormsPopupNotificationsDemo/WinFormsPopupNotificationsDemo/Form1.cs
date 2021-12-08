f34182b74b8482af4a2358eee5143f8e42039fdf
Pivanov Nikolay <it3@tstarter.ru>
2021-12-08 16:06:40 +0200

    Added to Git

---
+++ WinFormsPopupNotificationsDemo/WinFormsPopupNotificationsDemo/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Tulpep.NotificationWindow;
+
+namespace WinFormsPopupNotificationsDemo
+{
+    /*
+     Install nuget package Tulpep.NotificationWindow
+     */
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNotificationDemo_Click(object sender, EventArgs e)
+        {
+            PopupNotifier popup = new PopupNotifier()
+            {
+                TitleText = "Popup window",
+                ContentText = "Notification test",
+                Image = System.Drawing.SystemIcons.Information.ToBitmap(),
+                ImagePadding = new Padding(5),
+                Size = new Size(200, 100),
+                Delay = 1500
+            };
+
+            popup.Popup();
+        }
+    }
+}
